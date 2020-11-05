1858195f0a51201e80b593ec92eb82b2d9798a28
Stanimir Kolev <matematikata9@gmail.com>
2020-11-05 14:49:44 +0200

    initial commit

---
+++ 6okarNaP1k0/Modules/Commands.cs
@@ -0,0 +1,54 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6okarNaP1k0.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("kaksi")]
+        public async Task KakSi()
+        {
+            await ReplyAsync("6okar sym tenk u");        
+        }
+
+        [Command("laho")]
+        public async Task Laho()
+        {
+            Random random = new Random();
+            var id = random.Next(1, 100) % 3;
+            if (id == 0)
+            {
+                await Context.Channel.SendFileAsync("piko1.jpeg", "Na ti chubrica, gevreishyn!");
+            }
+            else if (id == 1)
+            {
+                await Context.Channel.SendFileAsync("piko2.png", "p1k0 de, ama nikoi nqq po - dylga liniq ot moqta!");
+            }
+            else
+            {
+                await Context.Channel.SendFileAsync("piko3.jpg", "Nai - dobroto maka6i!");
+            }
+        }
+
+        [Command("rechnik")]
+        public async Task Rechnik()
+        {
+            await ReplyAsync("Q gledai tuka e i dav tu bule, sekish! \n https://www.bgjargon.com/wordlist/tag/%D1%86%D0%B8%D0%B3%D0%B0%D0%BD%D1%81%D0%BA%D0%B8");
+        }
+
+        [Command("pomagai")]
+        public async Task Pomagai()
+        {
+            StringBuilder message = new StringBuilder();
+            message.AppendLine("Geldim, qsha batko! Q moga:\n");
+            message.AppendLine("Prefixa e \'!p1k0\', ne go zabraai!");
+            message.AppendLine("Ako sh mi pravish bibitki, pitai ma \'kaksi\'");
+            message.AppendLine("Ako sakash iznenada samo si poiskai s \'laho\'");
+            message.AppendLine("Ako si smotan i ne znaesh ciganski, glei u \'rechnik\'");
+            await ReplyAsync(message.ToString());
+        }
+    }
+}
---
+++ 6okarNaP1k0/Program.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using System.Security.Authentication.ExtendedProtection;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace _6okarNaP1k0
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        static void Main(string[] args)
+            => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "NzczODM1MzQyMjg4NjUwMjQw.X6PAUg.T_WOBrzEzJXXOHwgUsElE4n5O2A";
+            _client.Log += _client_Log;
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix("!p1k0 ", ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
